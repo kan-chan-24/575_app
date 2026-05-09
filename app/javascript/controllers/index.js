1dce56227272a6f121aa81ee9e5f402b2fd10b80
kan_chan <shunplay1793@gmail.com>
2026-05-10 02:35:19 +0900

    java再ぷりコンパイル

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,10 @@
-// Import and register all your controllers from the importmap under controllers/*
-
+// app/javascript/controllers/index.js
 import { application } from "./application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+// または、個別にインポートしている場合
+// import MoraeCounterController from "./morae_counter_controller"
+// application.register("morae-counter", MoraeCounterController)
