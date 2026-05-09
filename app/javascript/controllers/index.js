59af134aa5f4b16bfb6fbef3d02e70baa8096c64
kan_chan <shunplay1793@gmail.com>
2026-05-10 02:51:49 +0900

    Fix Stimulus controllers loading

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,7 +4,3 @@ import { application } from "./application"
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-// または、個別にインポートしている場合
-// import MoraeCounterController from "./morae_counter_controller"
-// application.register("morae-counter", MoraeCounterController)
