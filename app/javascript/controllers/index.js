5c1d276738b6745516cbd0552ce9bc8cb196df99
kan_chan <shunplay1793@gmail.com>
2026-05-10 17:04:41 +0900

    コンパイル設定を修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,4 @@
-// app/javascript/controllers/index.js
 import { application } from "./application"
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
